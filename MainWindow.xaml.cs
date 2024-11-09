2395e0b2dea20875683cece6866718c307cfa21f
ryode <ryode@DESKTOP-CJIPEKG>
2024-11-09 16:32:52 +0500

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int numberCount = 1;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            View.Items.Add(numberCount);
+            numberCount++;
+        }
+
+        private void Del_Click(object sender, RoutedEventArgs e)
+        {
+            if (View.SelectedItem != null)
+            {
+                View.Items.Remove(View.SelectedItem);
+            }
+        }
+    }
+}
